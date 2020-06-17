b7a579485727ceb36c3b2fec30f763f15af05f8f
LeanGomez99 <lea-kpo2011@hotmail.com>
2020-06-17 17:00:43 -0300

    Initial commit

---
+++ main.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once "clases/Persona.php";
+require_once "clases/Usuario.php";
+require_once "clases/Barrio.php";
+require_once "clases/Contacto.php";
+
+// guardar
+
+//
+
+
+
+/*
+$usuario = new Usuario('jose','gomez');
+$usuario->setUsername('josgo');
+$usuario->setPassword('555');
+$usuario->setFechaNacimiento('6245-03-25');
+//$persona->setIdTipoDocumento('tipoDocumento');
+$usuario->setNumeroDocumento('2332');
+$usuario->setSexo('m');
+
+
+$usuario->actualizar(20);
+
+
+$barrio = new Barrio();
+
+$barrio->setNombre('2 de abril');
+
+$barrio->actualizar(2);
+$barrio->guardar();
+$barrio->eliminar(1);
+
+$contacto = new contacto();
+
+$contacto->setDescripcion('falso@gmail.com');
+$contacto->settipoContacto('correo');
+
+$contacto->guardar();
+
+
+$domicilio = new Domicilio();
+$domicilio->setCasa(30);
+$domicilio->setCalle(307);
+$domicilio->guardar();
+
+*/
+
+highlight_string(var_export($usuario, true));
+
+?>
