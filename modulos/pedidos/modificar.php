e5d1c12310fe1dd8ded5c5de3dbb639f680a3732
LeanGomez99 <lea-kpo2011@hotmail.com>
2020-10-13 19:49:01 -0300

    Update modificar.php

--- modulos/pedidos/modificar.php
+++ modulos/pedidos/modificar.php
@@ -378,23 +378,23 @@ $listadoEstadoPedido = EstadoPedido::obtenerTodos();
 	//console.log(detallePedido);
 
 	function eliminarItem(indiceDelete){
-			let respuesta=[];
-			for (let index = 0; index < detallePedido.length; index++){
-				if(detallePedido[index].indice !== indiceDelete){
-					respuesta.push(detallePedido[indice])
-					//console.log(respuesta[index]);
-				} else {
-					console.log('borra este id');
-					console.log(index);
-					$('#' + detallePedido[index].indice).remove();
-					//restarSubTotal(detallePedido[index].subtotal);
-					//respuesta.splice(index, 1);
-				}
+		let respuesta=[];
+		for (let index = 0; index < detallePedido.length; index++){
+			if(detallePedido[index].indice !== indiceDelete){
+				respuesta.push(detallePedido[indice])
+				//console.log(respuesta[index]);
+			} else {
+				console.log('borra este id');
+				console.log(index);
+				$('#' + detallePedido[index].indice).remove();
+				//restarSubTotal(detallePedido[index].subtotal);
+				//respuesta.splice(index, 1);
 			}
-			//console.log(respuesta);
-			detallePedido = respuesta;
-			console.log(detallePedido);
-			return detallePedido;		
+		}
+		//console.log(respuesta);
+		detallePedido = respuesta;
+		console.log(detallePedido);
+		return detallePedido;		
 	}
 	
 	function guardarFormVentas(){
