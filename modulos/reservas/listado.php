20249da96fc5593ed083f8549cbbd72d418fdbe7
LeanGomez99 <lea-kpo2011@hotmail.com>
2020-10-22 17:30:45 -0300

    Se crea el calendario

---
+++ modulos/reservas/listado.php
@@ -0,0 +1,52 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Listado Reserva</title>
+</head>
+<body>
+
+	<?php require_once '../../menu.php'; ?>
+
+    <div class="main-content">
+        <div class="section__content section__content--p30">
+            <div>
+            <a class="au-btn au-btn-icon au-btn--blue" href="alta.php">
+                <i class="zmdi zmdi-plus"></i>Calendario de Reservas</a>
+            </div>
+            <br>                    
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="main-content">
+                        <div class="section__content section__content--p30">
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col">
+                                      <div class="au-card">
+                                        <div id="calendar"></div>
+                                      </div>
+                                    </div><!-- .col -->
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="copyright">
+                                            <p>Copyright © 2018 Colorlib. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
